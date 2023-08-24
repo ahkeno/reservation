1c5b2f079536835673cfdcc79eec1a7fdbdcd519
chit <ahkeno@gmail.com>
2023-08-24 17:32:12 +0630

    intial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script  setup lang="ts" >
+import RestuarantTypeVue from '../components/RestuarantType.vue';
+
+</script>
+<template>
+  <main>
+  <section>
+    this is Promotion Component
+  </section>
+  <section>
+    this is Top Reated Component
+  </section>
+  <section>
+    <RestuarantTypeVue/>
+  </section>
+</main>
+</template>
+<style scope>
+.home-view
+{
+  border: 1px solid red;
+}
+</style>
